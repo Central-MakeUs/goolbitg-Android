179e37ff78afd51aa74ea47732ccd7f4ebd6eb78
YuhoLee <57080476+YuhoLee@users.noreply.github.com>
2025-01-10 00:37:27 +0900

    Design: 디자인시스템 Color 및 Typography 추가

---
+++ presentation/src/main/java/com/project/presentation/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package com.project.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
+val transparent = Color(0x00FF0000)
+
+val primary900 = Color(0xFF016301)
+val primary800 = Color(0xFF268102)
+val primary700 = Color(0xFF339213)
+val primary600 = Color(0xFF41A420)
+val primary500 = Color(0xFF4BB329)
+val primary400 = Color(0xFF67BF4E)
+val primary300 = Color(0xFF83CA6F)
+val primary200 = Color(0xFFA7D899)
+val primary100 = Color(0xFFC9E7C1)
+val primary50 = Color(0xFFE9F5E6)
+
+val gray900 = Color(0xFF060606)
+val gray800 = Color(0xFF161616)
+val gray700 = Color(0xFF212121)
+val gray600 = Color(0xFF2F2F2F)
+val gray500 = Color(0xFF464646)
+val gray400 = Color(0xFF616161)
+val gray300 = Color(0xFF9E9E9E)
+val gray200 = Color(0xFFBDBDBD)
+val gray100 = Color(0xFFE0E0E0)
+val gray50 = Color(0xFFFAFAFA)
+
+val bg1 = Color(0xFF1E1E1E)
+val bg2 = Color(0xFFFAFAFA)
+
+val warning = Color(0xFFF0BB0D)
+val error = Color(0xFFF05D0D)
---
+++ presentation/src/main/java/com/project/presentation/ui/theme/Type.kt
@@ -0,0 +1,108 @@
+package com.project.presentation.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.project.presentation.R
+
+private val PretendFontFamily = FontFamily(
+    Font(R.font.pretendard_bold, weight = FontWeight.Bold),
+    Font(R.font.pretendard_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.pretendard_medium, weight = FontWeight.Medium),
+    Font(R.font.pretendard_regular, weight = FontWeight.Normal),
+)
+
+val raceOnTypography = GoolbitgTypography(
+    h1 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    h2 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 19.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    h3 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    body1 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 19.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    body2 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    body3 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    body4 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    caption1 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    caption2 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    btn1 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 19.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    btn2 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    ),
+    btn3 = TextStyle(
+        fontFamily = PretendFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
+    )
+)
+
+@Immutable
+data class GoolbitgTypography(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val body3: TextStyle,
+    val body4: TextStyle,
+    val caption1: TextStyle,
+    val caption2: TextStyle,
+    val btn1: TextStyle,
+    val btn2: TextStyle,
+    val btn3: TextStyle,
+)
