823e8deef973edfee483dbd65d0dd808c735a3c1
YuhoLee <cdcdefg4@inbody.com>
2025-02-09 05:17:12 +0900

    Fix: url 주소 수정

--- presentation/src/main/java/com/project/presentation/common/AgreementEnum.kt
+++ presentation/src/main/java/com/project/presentation/common/AgreementEnum.kt
@@ -1,13 +1,13 @@
 package com.project.presentation.common
 
 import androidx.annotation.StringRes
-import com.project.data.remote.common.BaseUrl.PRIVACY_AND_POLICY
-import com.project.data.remote.common.BaseUrl.TERMS_OF_SERVICES
+import com.project.data.remote.common.BaseUrl.PRIVACY_AND_POLICY_URL
+import com.project.data.remote.common.BaseUrl.TERMS_OF_SERVICES_URL
 import com.project.presentation.R
 
 enum class AgreementEnum(@StringRes val strId: Int, val webUrl: String? = null) {
     AgeFourteen(strId = R.string.onboarding_first_agreement_1),
-    TermsOfServices(strId = R.string.onboarding_first_agreement_2, webUrl = TERMS_OF_SERVICES),
-    PrivacyAndPolicy(strId = R.string.onboarding_first_agreement_3, webUrl = PRIVACY_AND_POLICY),
+    TermsOfServices(strId = R.string.onboarding_first_agreement_2, webUrl = TERMS_OF_SERVICES_URL),
+    PrivacyAndPolicy(strId = R.string.onboarding_first_agreement_3, webUrl = PRIVACY_AND_POLICY_URL),
     Advertisement(strId = R.string.onboarding_first_agreement_4)
 }
