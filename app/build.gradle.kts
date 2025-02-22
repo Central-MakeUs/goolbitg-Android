b510e42fb01f0f985a0bcd9a1fa9cbd99b3dd083
YuhoLee <cdcdefg4@gmail.com>
2025-02-23 01:49:24 +0900

    Revert "release: 1.0.2(2)"
    This reverts commit 0a59ce9c1949f0524c380f0c84f7789cfc835543.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 2
-        versionName = "1.0.2"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
